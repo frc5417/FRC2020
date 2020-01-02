17e7f8f1efaeded62c15a1f25d0969e93a72691b
AtikshB <atikshbhardwaj@gmail.com>
2020-01-02 17:28:58 -0600

    First Commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
+  private static final String kDefaultAuto = "Default";q
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
