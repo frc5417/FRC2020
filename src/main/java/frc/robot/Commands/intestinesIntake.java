95b374fb28713062b9f7181aa952e4315a289d64
Lucas-Vaughan <lucas.vaughan@student.allenisd.org>
2020-01-30 20:30:34 -0600

    Fixed Nimo's buggy setup code

--- src/main/java/frc/robot/Commands/intestinesIntake.java
+++ src/main/java/frc/robot/Commands/intestinesIntake.java
@@ -1,8 +1,12 @@
 package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-public class intestinesIntake extends Command {
-    
+public class IntestinesIntake extends CommandBase {
+
+    public IntestinesIntake(){
+
+    }
 }
