d58b473b4d5171243a866dfb3afcccb7f1b932c0
starship007 <thisiscodyr@gmail.com>
2020-02-11 20:43:37 -0600

    reconfirmed that color_GD works

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 //cody's comment
-//cody's comment for git color_GD
+//cody's comment for git color_GD, recomfirmation
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
